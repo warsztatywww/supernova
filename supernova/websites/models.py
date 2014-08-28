66dca67a1f07f528fd8d97cfd177d587f84e2d67
Patryk Hes <patryk.hes@laboratorium.ee>
2014-08-28 02:18:14 +0200

    celery working on project level, crawl_and_parse can crawl

--- supernova/websites/models.py
+++ supernova/websites/models.py
@@ -5,6 +5,9 @@ class Domain(models.Model):
     name = models.CharField(max_length=255)
     pagerank = models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 
 class Webpage(models.Model):
     domain = models.ForeignKey(Domain)
@@ -14,7 +17,13 @@ class Webpage(models.Model):
     keywords = models.CharField(max_length=1023)
     content = models.TextField()
 
+    def __str__(self):
+        return '{}/{}'.format(self.domain, self.path)
+
 
 class Link(models.Model):
     start = models.ForeignKey(Webpage, related_name='link_start')
     end = models.ForeignKey(Webpage, related_name='link_end')
+
+    def __str__(self):
+        return '{} -> {}'.format(self.start, self.end)
