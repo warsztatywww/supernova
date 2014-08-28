7f196f6c1544697718112a73270e9e48aa84d983
Piotr Migdal <pmigdal@gmail.com>
2014-08-28 12:09:36 +0200

    json

--- supernova/graph/urls.py
+++ supernova/graph/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = patterns(
     '',
-    url(r'^', graph_view, name='graph'),
+    url(r'^$', graph_view, name='graph'),
     url(r'^json$', graph_json, name='graph_json'),
 )
