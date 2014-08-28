0fce1721c913085bd04d681f6c2de6e8c917e63b
Piotr Migdal <pmigdal@gmail.com>
2014-08-28 11:13:22 +0200

    viz urls

--- supernova/graph/views.py
+++ supernova/graph/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from supernova.websites.models import Domain, Webpage
+import json
 
 def graph_view(request):
     return render_to_response(
         'graph.html',
         {},
     )
+
+def graph_json(request):
+    data = {"nodes": [],
+            "links": []}
+    return HttpResponse(json.dumps(data), content_type="application/json")
--- supernova/supernova/urls.py
+++ supernova/supernova/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^graph', include('graph.urls')),
     url(r'^', include('gui.urls')),
 )
