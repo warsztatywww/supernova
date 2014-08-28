b3c7993d6e81c4fb4f0c633aee16ceb22d3219d8
kam193 <kam193@wp.pl>
2014-08-28 10:34:28 +0200

    przeniesione do /index

--- supernova/index/indexr/config.py
+++
@@ -1,8 +0,0 @@
-# adres serwera redis
-redis_server_ip = "127.0.0.1"
-
-# port serwera redis - nieużywane
-#redis_server_port = 6379
-
-# czas przechowywania danych o odwiedzaniu strony (sekundy)
-czas_do_ponownego_odwiedzenia_strony = 300
